e07967050ea345b04f3bd16ff839914e679522a7
Hao-Cher Hong <rax333j@gmail.com>
2014-01-25 20:33:21 +0800

    Alpha 1.0

---
+++ Assets/Scripts/Targets/ThornTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThornTarget : Target {
+
+    public override bool OnMirrored()
+    {
+        base.OnMirrored();
+        isHarmful = false;
+        return true;
+    }
+}
