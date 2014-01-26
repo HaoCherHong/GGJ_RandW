f464c8a3c791a16614f7e85a325edfee6255470b
Hao-Cher Hong <rax333j@gmail.com>
2014-01-26 12:24:17 +0800

    Level 1 Updated

---
+++ Assets/Scripts/Targets/AnimatedTarget.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedTarget : Target 
+{
+    public override bool OnMirrored()
+    {
+        base.OnMirrored();
+
+        animation[animation.clip.name].speed *= -1;
+
+        return true;
+    }
+}
