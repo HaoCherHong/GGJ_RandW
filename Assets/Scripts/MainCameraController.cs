057ac00f0b931342b98e558a6ce30124bcd9a02b
Hao-Cher Hong <rax333j@gmail.com>
2014-01-28 22:17:50 +0800

    MainCamera Updated
    Manually Update position

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraController : MonoBehaviour {
+
+    public GameCharacterController character;
+    Vector3 positionDelta;
+
+	// Use this for initialization
+	void Start () {
+        positionDelta = transform.position - character.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = character.transform.position + positionDelta;
+	}
+}
