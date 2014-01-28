fe97723979092f10267fd6ac947e94d88dabba7a
williamd4112 <williamd4112@hotmail.com>
2014-01-28 21:01:45 +0800

    Detection script

---
+++ Assets/Scripts/DetectionCommon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static class DetectionCommon
+{
+	static public bool ContainmentTest(Bounds container , Bounds obj)
+	{
+
+			if(container.center.x + container.extents.x >= obj.center.x + obj.extents.x
+			 &&container.center.x - container.extents.x <= obj.center.x - obj.extents.x
+			 &&container.center.y + container.extents.y >= obj.center.y + obj.extents.y
+			 &&container.center.y - container.extents.y <= obj.center.y - obj.extents.y)
+			 return true;
+		    else
+			 return false;
+	
+	}
+
+}
