c7a9f5f2850df53e1fca51d91d8e991e0e91c73b
Hao-Cher Hong <rax333j@gmail.com>
2014-01-29 23:16:06 +0800

    LensesMenu
    Right-Click LensesMenu feature added

---
+++ Assets/Scripts/LensesMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LensesMenu : MonoBehaviour 
+{
+    public GameController gameController;
+    public LensesMenuItem[] subItems;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Open()
+    {
+        //Update Position
+        Plane plane = new Plane(-Vector3.forward, 0);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayDistance;
+        plane.Raycast(ray, out rayDistance);
+        Vector3 hit = ray.GetPoint(rayDistance);
+        transform.position = hit;
+
+        foreach (LensesMenuItem item in subItems)
+            item.OnMenuOpened();
+    }
+
+    public void Close()
+    {
+        foreach (LensesMenuItem item in subItems)
+            item.OnMenuClosed();
+    }
+
+    public void OnParentSelected(LensesMenuItem selectedItem)
+    {
+        foreach (LensesMenuItem item in subItems)
+            if (item != selectedItem)
+                item.visible = false;
+    }
+
+    public void OnModeSelected(SecondCameraController.CameraMode mode)
+    {
+        gameController.SetCameraMode(mode);
+    }
+}
---
+++ Assets/Scripts/LensesMenuItem.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LensesMenuItem : MonoBehaviour {
+    public LensesMenu menu;
+
+    public SecondCameraController.CameraMode mode;
+    public bool isParent;
+    public LensesMenuItem[] subItems;
+
+    public bool visible = false;
+    Vector3 localScaleCurrentVelocity;
+    float targetScale = 1.0f;
+
+    bool isSelected = false;
+	// Use this for initialization
+	void Start () 
+    {
+	    if(isParent)
+        {
+            foreach (LensesMenuItem item in subItems)
+                item.visible = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.localScale = Vector3.SmoothDamp(transform.localScale, Vector3.one * targetScale * (visible ? 1.0f : 0.0f), ref localScaleCurrentVelocity, 0.05f);
+	}
+
+    
+
+    void OnMouseEnter()
+    {
+        if (isParent)
+        {
+            menu.OnParentSelected(this);
+
+            foreach (LensesMenuItem item in subItems)
+                item.visible = true;
+
+            
+        }
+
+        if (mode != SecondCameraController.CameraMode.None)
+            menu.OnModeSelected(mode);
+
+        targetScale = 1.2f;
+    }
+
+    void OnMouseExit()
+    {
+
+        targetScale = 1.0f;
+    }
+
+    public void OnMenuOpened()
+    {
+        visible = true;
+    }
+
+    public void OnMenuClosed()
+    {
+        visible = false;
+        if (isParent)
+        {
+            foreach (LensesMenuItem item in subItems)
+                item.visible = false;
+        }
+    }
+}
