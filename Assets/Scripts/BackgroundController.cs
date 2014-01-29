4d2354ddd6daf9273a646321e89a563f7cff8feb
Hao-Cher Hong <rax333j@gmail.com>
2014-01-29 23:16:47 +0800

    Background Rendering Update

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+    public Vector2 offsetTransition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.material.mainTextureOffset = renderer.material.mainTextureOffset + offsetTransition * Time.deltaTime;
+	}
+}
